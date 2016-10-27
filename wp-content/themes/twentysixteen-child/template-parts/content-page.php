5fee66f19a84b513d2c884c4f32c4959fab45307
Maria Daniel Deepak <mariadanieldeepak@gmail.com>
2016-10-27 15:54:33 +0530

    Include conditional checks before using Custom fields

--- wp-content/themes/twentysixteen-child/template-parts/content-page.php
+++ wp-content/themes/twentysixteen-child/template-parts/content-page.php
@@ -22,70 +22,63 @@
 		// Check if ACF Modules are present.
 		if ( get_field( 'acf_modules' ) ) :
 
-            /*
-            $custom_fields = get_fields();
-            $flexible_fields = $custom_fields["acf_modules"];
-
-            $acf_modules = array();
-            foreach( $flexible_fields as $flexible_field ) {
-                $acf_modules[] = $flexible_field['acf_fc_layout'];
-            }
-            */
-
             // If present, display the modules
             while ( has_sub_field( 'acf_modules' ) ) :
 
-                if ( get_row_layout() == 'page_anchors' ) {
-                    echo "<div class='module'>";
-
-                    if ( have_rows( 'menu' ) ) :
-                        while( have_rows('menu') ) : the_row(); ?>
-                            <a class="page-anchor" href="<?php the_sub_field( 'url' ); ?>"><?php the_sub_field( 'label' ); ?></a>
-                        <?php
-                        endwhile;
-                    endif;
-
-                    echo "</div>";
-                }
-
-                if (get_row_layout() == 'single_left_image_with_text' ) : ?>
+				if ( get_row_layout() == 'single_left_image_with_text' ) : ?>
 
 					<div class='module'>
-						<h2><?php the_sub_field( 'text' ); ?></h2>
-						<img src="<?php the_sub_field('image'); ?>" />
-						<p><?php the_sub_field( 'description' ); ?></p>
-						<span><a href="<?php the_sub_field( 'cta_url' ); ?>"><?php the_sub_field( 'cta_label' ); ?></a></span>
-                    </div>
+
+						<?php if ( get_sub_field( 'text' ) ): ?>
+
+							<h2><?php the_sub_field( 'text' ); ?></h2>
+
+						<?php endif; ?>
+
+						<?php if ( get_sub_field( 'image' ) ): ?>
+
+							<img src="<?php the_sub_field('image'); ?>" />
+
+						<?php endif; ?>
+
+						<?php if ( get_sub_field( 'description' ) ): ?>
+
+							<p><?php the_sub_field( 'description' ); ?></p>
+
+						<?php endif; ?>
+
+						<?php if ( get_sub_field( 'cta_url' ) && get_sub_field( 'cta_label' ) ): ?>
+
+							<span><a href="<?php the_sub_field( 'cta_url' ); ?>"><?php the_sub_field( 'cta_label' ); ?></a></span>
+
+						<?php endif; ?>
+
+					</div>
 
 					<?php
-                endif;
+				endif;
+
+                if ( get_row_layout() == 'page_anchors' ) {
+
+                    if ( have_rows( 'menu' ) ) :
+						echo "<div class='module'>";
+
+						while( have_rows('menu') ) : the_row();
+
+							if ( get_sub_field( 'url' ) && get_sub_field( 'label' ) ) : ?>
+								<a class="page-anchor" href="<?php the_sub_field( 'url' ); ?>"><?php the_sub_field( 'label' ); ?></a>
+							<?php
+							endif;
+
+						endwhile;
+
+						echo "</div>";
+					endif;
+
+                }
 
             endwhile;
 
-            /*
-            $custom_fields = get_fields();
-            $acf_modules = $custom_fields["acf_modules"];
-
-            //var_dump($acf_modules);
-
-            foreach( $acf_modules as $acf_module ) {
-                echo $acf_module["acf_fc_layout"] . "<br />";
-            }
-            */
-            /*
-            while ( has_sub_field( 'acf_modules' ) ) {
-
-                if( get_row_layout() == 'page_anchors' ) {
-                }
-                if( get_row_layout() == 'test' ) {
-                    echo "<div class='module'>" . get_row_index() ."</div>";
-                }
-        ?>
-
-        <?php }*/ // End while
-
-		else :
-			echo "0";
 		endif;
 
 		the_content();
